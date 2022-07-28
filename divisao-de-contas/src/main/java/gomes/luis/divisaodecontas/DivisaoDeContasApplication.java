adfd4d8d9922704ac24104639e3f8ef21e710e4a
luis.gomes <luis.gomes@objective.com.br>
2022-07-28 17:45:25 -0300

    primeiro commit - subindo projeto...

---
+++ divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/DivisaoDeContasApplication.java
@@ -0,0 +1,13 @@
+package gomes.luis.divisaodecontas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivisaoDeContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivisaoDeContasApplication.class, args);
+	}
+
+}
---
+++ divisao-de-contas/src/test/java/gomes/luis/divisaodecontas/DivisaoDeContasApplicationTests.java
@@ -0,0 +1,13 @@
+package gomes.luis.divisaodecontas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivisaoDeContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
