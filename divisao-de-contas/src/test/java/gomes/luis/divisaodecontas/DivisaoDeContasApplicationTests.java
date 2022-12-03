d3f6bc6a2a6323d79ceb02359c6892e6747384a3
luis.gomes <luis.gomes@objective.com.br>
2022-12-03 06:51:46 -0300

    configuração para testes

--- divisao-de-contas/src/test/java/gomes/luis/divisaodecontas/DivisaoDeContasApplicationTests.java
+++ divisao-de-contas/src/test/java/gomes/luis/divisaodecontas/DivisaoDeContasApplicationTests.java
@@ -1,10 +1,15 @@
 package gomes.luis.divisaodecontas;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class DivisaoDeContasApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DivisaoDeContasApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:application.properties")
+public class DivisaoDeContasApplicationTests {
 
 	@Test
 	void contextLoads() {
