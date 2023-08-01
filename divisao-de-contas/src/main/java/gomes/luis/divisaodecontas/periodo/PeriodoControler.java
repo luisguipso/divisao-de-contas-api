53a2d5878ef473d97da170c708da14d7f79161bd
luis.gomes <luis.gomes@objective.com.br>
2023-08-01 10:43:25 -0300

    add calcularValorTotal on periodo api

--- divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/periodo/PeriodoControler.java
+++ divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/periodo/PeriodoControler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,37 +18,43 @@ public class PeriodoControler {
 
     PeriodoService periodoService;
 
-    PeriodoControler(PeriodoService periodoService){
+    PeriodoControler(PeriodoService periodoService) {
         this.periodoService = periodoService;
     }
 
     @GetMapping
-    public ResponseEntity<List<Periodo>> buscarTodosOsPeriodos(){
+    public ResponseEntity<List<Periodo>> buscarTodosOsPeriodos() {
         List<Periodo> periodos = periodoService.buscarTodosOsPeriodos();
         return new ResponseEntity<>(periodos, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Periodo> buscarPeriodoPorId(@PathVariable(name = "id") Long id){
+    public ResponseEntity<Periodo> buscarPeriodoPorId(@PathVariable(name = "id") Long id) {
         Optional<Periodo> periodo = periodoService.buscarPorId(id);
         return new ResponseEntity<>(periodo.orElse(null), HttpStatus.OK);
     }
 
+    @GetMapping("/{id}/valorTotal")
+    public ResponseEntity<BigDecimal> buscarValorTotalPeriodo(@PathVariable(name = "id") Long id) {
+        Periodo periodo = periodoService.buscarPorId(id).orElseThrow();
+        return new ResponseEntity<>(periodoService.calcularValorTotal(periodo), HttpStatus.OK);
+    }
+
     @PostMapping()
-    public ResponseEntity<String> salvarPeriodo(@RequestBody Periodo periodo){
+    public ResponseEntity<String> salvarPeriodo(@RequestBody Periodo periodo) {
         periodoService.salvar(periodo);
-        return new ResponseEntity<>(PERIODO_CRIADO,HttpStatus.CREATED);
+        return new ResponseEntity<>(PERIODO_CRIADO, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> atualizarPeriodo(@PathVariable Long id, @RequestBody Periodo periodo){
+    public ResponseEntity<String> atualizarPeriodo(@PathVariable Long id, @RequestBody Periodo periodo) {
         periodoService.atualizar(id, periodo);
-        return new ResponseEntity<>(PERIODO_ATUALIZADO,HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(PERIODO_ATUALIZADO, HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> excluirPeriodo(@PathVariable Long id){
+    public ResponseEntity<String> excluirPeriodo(@PathVariable Long id) {
         periodoService.excluirPorId(id);
-        return new ResponseEntity<>(PERIODO_EXCLUIDO,HttpStatus.OK);
+        return new ResponseEntity<>(PERIODO_EXCLUIDO, HttpStatus.OK);
     }
 }
--- divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/periodo/PeriodoService.java
+++ divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/periodo/PeriodoService.java
@@ -1,6 +1,8 @@
 package gomes.luis.divisaodecontas.periodo;
 
 
+import gomes.luis.divisaodecontas.despesa.Despesa;
+import gomes.luis.divisaodecontas.despesa.DespesaService;
 import gomes.luis.divisaodecontas.service.GenericService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,8 +14,10 @@ import java.util.List;
 @EnableTransactionManagement
 public class PeriodoService extends GenericService<Periodo, Long> {
 
-    public PeriodoService(PeriodoRepository repository) {
+    private final DespesaService despesaService;
+    public PeriodoService(PeriodoRepository repository, DespesaService despesaService) {
         super(repository);
+        this.despesaService = despesaService;
     }
 
     public List<Periodo> buscarTodosOsPeriodos() {
@@ -27,5 +31,13 @@ public class PeriodoService extends GenericService<Periodo, Long> {
         periodo.setValorTotal(valorTotal);
         atualizar(periodo.getId(), periodo);
         return periodo;
+    }
+
+    public BigDecimal calcularValorTotal(Periodo periodo) {
+        List<Despesa> despesasDoPeriodo = this.despesaService.buscarDespesasPorPeriodo(periodo.getId());
+        return despesasDoPeriodo.stream()
+                .map(Despesa::getValor)
+                .reduce(BigDecimal::add)
+                .orElseGet(() -> new BigDecimal(0));
     }
 }
