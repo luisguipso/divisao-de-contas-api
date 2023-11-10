5c384d1bb2fd6edabdca4e2178370450eb53f0e6
luis.gomes <luis.gomes@objective.com.br>
2023-11-10 10:41:44 -0300

    create two different filters for /login and /api

--- divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/security/SecurityConfiguration.java
+++ divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package gomes.luis.divisaodecontas.security;
 import gomes.luis.divisaodecontas.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,7 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -22,12 +23,26 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtFilter) throws Exception {
-        return http.headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+    @Order(1)
+    public SecurityFilterChain apiFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtFilter) throws Exception {
+        http.securityMatcher("/api/**");
+        http.headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        http.cors(cors -> cors.configure(http));
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.sessionManagement(sm -> sm.sessionCreationPolicy(STATELESS));
+        http.addFilterBefore(jwtFilter, BasicAuthenticationFilter.class);
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/**").authenticated());
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain loginFilterChain(HttpSecurity http) throws Exception {
+        return http.securityMatcher("/login/**")
+                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .cors(cors -> cors.configure(http))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(STATELESS))
-                .addFilterBefore(jwtFilter, BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .build();
     }
@@ -35,7 +50,9 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:4200"));
+        configuration.setAllowCredentials(true);
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
