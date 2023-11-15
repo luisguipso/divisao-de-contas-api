1ce3ed7c6d6a54905099486ea5542f45533f31d4
luis.gomes <luis.gomes@objective.com.br>
2023-11-15 15:06:19 -0300

    Remove unused code

--- divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/extrato/usuario/ExtratoPorUsuarioRepository.java
+++ divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/extrato/usuario/ExtratoPorUsuarioRepository.java
@@ -1,6 +1,5 @@
 package gomes.luis.divisaodecontas.extrato.usuario;
 
-import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,32 +17,6 @@ public interface ExtratoPorUsuarioRepository extends JpaRepository<ValorPorUsuar
             """)
     List<ValorPorUsuario> buscarValorPagoPorUsuarioNoPeriodo(Long periodoId);
 
-    @Query(value = """
-            WITH totalDevidoPorUsuarioNoPeriodo AS (
-                SELECT u.id, u.nome, u.percentual as percentual, SUM(d.valor) * (u.percentual / 100) AS valor
-                FROM despesa d
-                         JOIN periodo p ON p.id = d.id_periodo
-                         JOIN pagadores_dos_periodos pp ON pp.id_periodo = p.id
-                         JOIN pessoa u ON u.id = pp.id_pagador
-                WHERE d.id_periodo = :periodoId
-                AND d.is_divisivel = true
-                GROUP BY u.id
-                union all
-                SELECT u.id, u.nome, 100 as percentual, SUM(d.valor) AS valor
-                FROM despesa d
-                         JOIN periodo p ON p.id = d.id_periodo
-                         JOIN pessoa u ON u.id = d.id_dono
-                WHERE d.id_periodo = :periodoId
-                  AND d.is_divisivel = false
-                GROUP BY u.id
-            )
-            SELECT id, nome , AVG(percentual), SUM(valor)
-            FROM totalDevidoPorUsuarioNoPeriodo
-            GROUP BY id, nome""",
-            nativeQuery = true)
-    List<Tuple> buscarValorDevidoPorUsuarioNoPeriodo(Long periodoId);
-
-
     @Query("""
             SELECT new gomes.luis.divisaodecontas.extrato.usuario.ValorPorUsuario(d.dono , sum(d.valor))
                 FROM Despesa d
@@ -51,7 +24,8 @@ public interface ExtratoPorUsuarioRepository extends JpaRepository<ValorPorUsuar
                          JOIN Pessoa u ON u.id = d.dono.id
                 WHERE d.periodo.id = :periodoId
                   AND d.isDivisivel = false
-                GROUP BY u.id""")
+                GROUP BY u.id
+                ORDER BY u.id""")
     List<ValorPorUsuario> buscarValoresIndividuaisDevidosPorUsuarioNoPeriodo(Long periodoId);
 
     @Query("""
@@ -61,6 +35,7 @@ public interface ExtratoPorUsuarioRepository extends JpaRepository<ValorPorUsuar
             JOIN p.pagadores pp
             WHERE d.periodo.id = :periodoId
             AND d.isDivisivel = true
-            GROUP BY pp.id""")
+            GROUP BY pp.id
+            ORDER BY pp.id""")
     List<ValorPorUsuario> buscarValoresDividosDevidosPorUsuarioNoPeriodo(Long periodoId);
 }
