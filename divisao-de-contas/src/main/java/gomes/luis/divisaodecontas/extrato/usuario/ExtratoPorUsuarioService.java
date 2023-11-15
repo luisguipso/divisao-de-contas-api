abf15cfd90158b4870b2f017a6d944bf24f0505c
luis.gomes <luis.gomes@objective.com.br>
2023-11-15 13:30:13 -0300

    update ExtratoPorUsuarioService.buscarValorDevidoPorUsuarioNoPeriodo to fetch the data from 2 different methods and join it.

--- divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/extrato/usuario/ExtratoPorUsuarioService.java
+++ divisao-de-contas/src/main/java/gomes/luis/divisaodecontas/extrato/usuario/ExtratoPorUsuarioService.java
@@ -2,26 +2,33 @@ package gomes.luis.divisaodecontas.extrato.usuario;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ExtratoPorUsuarioService {
 
-    private final ExtratoPorUsuarioRepository extratoPorUsuarioRepository;
+    private final ExtratoPorUsuarioRepository repository;
 
-    public ExtratoPorUsuarioService(ExtratoPorUsuarioRepository extratoPorUsuarioRepository){
-        this.extratoPorUsuarioRepository = extratoPorUsuarioRepository;
+    public ExtratoPorUsuarioService(ExtratoPorUsuarioRepository repository){
+        this.repository = repository;
     }
     public List<ValorPorUsuario> buscarValorPagoPorUsuarioNoPeriodo(Long periodoId) {
-        return extratoPorUsuarioRepository.buscarValorPagoPorUsuarioNoPeriodo(periodoId);
+        return repository.buscarValorPagoPorUsuarioNoPeriodo(periodoId);
     }
 
     public List<ValorPorUsuario> buscarValorDevidoPorUsuarioNoPeriodo(Long periodoId){
-        TupleToValorPorUsuario converter = new TupleToValorPorUsuario();
-        return extratoPorUsuarioRepository.buscarValorDevidoPorUsuarioNoPeriodo(periodoId)
-                .stream()
-                .map(converter::convert)
-                .toList();
+        List<ValorPorUsuario> valoresDividosDevidos = repository.buscarValoresDividosDevidosPorUsuarioNoPeriodo(periodoId);
+        List<ValorPorUsuario> valoresIndividuaisDevidos = repository.buscarValoresIndividuaisDevidosPorUsuarioNoPeriodo(periodoId);
+        List<ValorPorUsuario> reduce = new ArrayList<>();
+        for(ValorPorUsuario divido : valoresDividosDevidos){
+            for(ValorPorUsuario individual : valoresIndividuaisDevidos){
+                if(divido.getUsuario().equals(individual.getUsuario()))
+                    divido.setValorTotal(divido.getValorTotal().add(individual.getValorTotal()));
+                reduce.add(divido);
+            }
+        }
+        return reduce;
     }
 
 
